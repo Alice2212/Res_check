2da2d9119625cf58e1022b16e4812f267c298d22
Oluwatosin <Hezealice@gmail.com>
2020-03-27 16:37:29 +0100

    1

--- controller/user.js
+++ controller/user.js
@@ -5,35 +5,7 @@ exports.loginPage = (req, res) =>{
     res.render('users/login');
 }
 
-// to check if user exist and retrieve data
-// exports.loginResult = (req, res) => {
-//     user.findById(req.params.id, function (err,result){
-//         if(err){
-//             req.flash ('success', ' ${user.username} does not exist')
-//             res.redirect('/')
-//         }else{
-//             // code for returning the result
-//             res.json({
-//                 message: "Your result is ",
-//                 data: user
 
-//             })
-//         }
-//     })
-// }
-
-// to check if user exit and retrieve data
-// exports.loginResult = (req,res)=>{
-//     user.findById(req.params.id, function(err,result){
-//         if (err){
-//             res.flash('User not found')
-//             res.redirect('/')
-//         }
-//         res.json({
-//             data : result
-//         })
-//     })
-// }
 
 exports.loginResult = (req,res)=>{
     user.findUserByUsername(username, function(user,result){
