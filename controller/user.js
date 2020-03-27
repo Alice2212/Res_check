d0db953583ec0b27b883254d4f4b9855c913e3e1
Oluwatosin <Hezealice@gmail.com>
2020-03-27 16:42:31 +0100

    finding user

--- controller/user.js
+++ controller/user.js
@@ -7,16 +7,31 @@ exports.loginPage = (req, res) =>{
 
 
 
-exports.loginResult = (req,res)=>{
-    user.findUserByUsername(username, function(user,result){
-        if(!user){
-            res.flash('User not Found')
-            res.redirect('/')
-        }
-        res.json({
-            data : result
-        })
+// exports.loginResult = (req,res)=>{
+//     user.findUserByUsername(username, function(user,result){
+//         if(!user){
+//             res.flash('User not Found')
+//             res.redirect('/')
+//         }
+//         res.json({
+//             data : result
+//         })
 
-    })
+//     })
+// }
+
+exports.loginResult = (req,res) => {
+    user.find({name: user.username})
+ .then((doc)=>{
+    console.log(doc);
+    res.json(doc);
+ })
+.catch((err)=>{
+    console.log(err);
+    res.flash ('User not found')
+    res.render('/users')
+});
 }
+
+
     
